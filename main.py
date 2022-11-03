2e7907dbcb8ebaeb9fb0b63724cd481b6e31f535
jacob lourie <jakelourie1502@gmail.com>
2022-11-03 13:19:43 +0000

    adds layers too

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
+from utils import Layer
 class me:
     def __init__(self):
         self.x = [1,2,3,4,5]
         self.y = self.sigmoid([a for a in self.x])
-
+        self.layer = Layer
 
     def sigmoid(self, x):
         return 1/ (1 + np.exp(-x))
 
+#
