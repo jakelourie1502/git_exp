4235c0a766d463b4524a47ccd8753e6ffb99ca64
jacob lourie <jakelourie1502@gmail.com>
2022-11-03 13:11:30 +0000

    dfdfd

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import numpy as np
 class me:
     def __init__(self):
         self.post_init()
 
+    def sigmoid(self, x):
+        return 1/ (1 + np.exp(-x))
+
     def post_init(self):
-        self.var1 = 1
-        self.var2 = 2
-        self.var3 = 3
-        self.var4 = 4
+        self.var1 = self.sigmoid(1)
+        self.var2 = self.sigmoid(2)
+        self.var3 = self.sigmoid(3)
+        self.var4 = self.sigmoid(4)
+        self.var5 = self.sigmoid(5)
