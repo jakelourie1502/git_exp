ce249710e4d9c4e7a88aba089f9ff49a01c33163
jacob lourie <jakelourie1502@gmail.com>
2022-11-03 14:13:59 +0000

    adding months

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
-from utils import Layer
+from utils import Layer, MONTHS
 class me:
     def __init__(self):
         self.x = [1,2,3,4,5]
         self.y = self.tanh([a for a in self.x])
         self.layer = Layer
+        self.layer2 = MONTHS
 
     def tanh(self, x):
         return (np.exp(x)-np.exp(-x))/ (np.exp(x)+np.exp(-x))
